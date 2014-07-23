a3cb7079443bbf9d2b7dfacf371ebe3e781422f6
Dmitry Koplyarov <koplyarov.da@gmail.com>
2014-07-24 03:07:23 +0400

    Made Location string representable

--- hide/Location.h
+++ hide/Location.h
@@ -2,7 +2,12 @@
 #define HIDE_LOCATION_H
 
 
+#include <iostream>
 #include <string>
+
+#include <boost/lexical_cast.hpp>
+
+#include <hide/Utils.h>
 
 
 namespace hide
@@ -20,8 +25,8 @@ namespace hide
         	: _filename(), _line(0), _column(0)
         { }
 
-	    Location(const std::string& filename, size_t line, size_t column)
-        	: _filename(filename), _line(line), _column(column)
+		Location(const std::string& filename, size_t line, size_t column)
+			: _filename(filename), _line(line), _column(column)
         { }
 
         bool IsValid() const { return !_filename.empty(); }
@@ -29,7 +34,11 @@ namespace hide
         std::string GetFilename() const	{ return _filename; }
         size_t GetLine() const			{ return _line; }
         size_t GetColumn() const		{ return _column; }
+
+		HIDE_DECLARE_TO_STRING_METHOD()
 	};
+
+	HIDE_DECLARE_WRITE_TO_OSTREAM(Location, s << v.GetFilename() << ':' << (v.GetLine()) << ':' << (v.GetColumn()); )
 
 }
 
