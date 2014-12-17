92d31dd09333ce40023297b7f84f463dca141b76
Dmitry Koplyarov <koplyarov.da@gmail.com>
2014-12-17 05:01:02 +0300

    Working on Executable class

--- hide/utils/Executable.cpp
+++ hide/utils/Executable.cpp
@@ -6,10 +6,107 @@
 #include <unistd.h>
 
 #include <sstream>
+#include <thread>
+
+#include <boost/scope_exit.hpp>
+
+#include <hide/utils/ReadBufferBase.h>
 
 
 namespace hide
 {
+
+	namespace
+	{
+#if HIDE_PLATFORM_POSIX
+		class PipeReadBuffer : public ReadBufferBase
+		{
+			HIDE_NONCOPYABLE(PipeReadBuffer);
+
+		private:
+			static NamedLogger	s_logger;
+			int					_fd;
+			ByteArray			_data;
+			std::thread			_thread;
+
+		public:
+			PipeReadBuffer(int fd)
+				: _fd(fd)
+			{
+				//fcntl(out_pipe[read_index], F_SETFL, O_NONBLOCK | O_ASYNC); // TODO: check errors
+				_thread = std::thread(std::bind(&PipeReadBuffer::ThreadFunc, this));
+			}
+
+			~PipeReadBuffer()
+			{
+				::close(_fd);
+				_thread.join();
+			}
+
+			virtual ByteArray Read(int64_t ofs) const
+			{
+				std::lock_guard<std::recursive_mutex> l(_mutex);
+				if (ofs > _data.size())
+					BOOST_THROW_EXCEPTION(std::runtime_error("Invalid offset!"));
+				ByteArray result;
+				int64_t result_size = _data.size() - ofs;
+				result.resize(result_size);
+				std::copy(_data.begin() + ofs, _data.begin() + ofs + result_size, result.begin());
+				return result;
+			}
+
+		private:
+			void ThreadFunc()
+			{
+				std::array<char, 256> local_buf;
+				int ret = 0;
+				do
+				{
+					ret = read(_fd, local_buf.data(), local_buf.size());
+					if (ret < 0)
+					{
+						if (errno == EBADF)
+							break;
+						else
+							BOOST_THROW_EXCEPTION(std::runtime_error("read failed!"));
+					}
+					else if (ret > 0)
+					{
+						std::lock_guard<std::recursive_mutex> l(_mutex);
+						_data.resize(_data.size() + ret);
+						std::copy(local_buf.begin(), local_buf.begin() + ret, _data.end() - ret);
+						for (auto l : _listeners)
+							l->OnBufferChanged(*this);
+					}
+				} while (ret >= 0);
+			}
+		};
+		HIDE_DECLARE_PTR(PipeReadBuffer);
+		HIDE_NAMED_LOGGER(PipeReadBuffer);
+#endif
+	}
+
+	class TestReadBufferListener : public virtual IReadBufferListener
+	{
+	private:
+		static NamedLogger	s_logger;
+		int64_t				_ofs;
+
+	public:
+		TestReadBufferListener()
+			: _ofs(0)
+		{ }
+
+		virtual void OnBufferChanged(const IReadBuffer& buf)
+		{
+			ByteArray data = buf.Read(_ofs);
+			_ofs += data.size();
+			data.push_back('\0');
+			printf("%s", data.data());
+		}
+	};
+	HIDE_NAMED_LOGGER(TestReadBufferListener);
+
 
 	HIDE_NAMED_LOGGER(Executable);
 
@@ -48,6 +145,11 @@ namespace hide
 			close(out_pipe[write_index]);
 			close(err_pipe[write_index]);
 
+			IReadBufferPtr out_buf(new PipeReadBuffer(out_pipe[read_index]));
+			out_buf->AddListener(std::make_shared<TestReadBufferListener>());
+			_stdout = out_buf;
+			_stderr.reset(new PipeReadBuffer(err_pipe[read_index]));
+
 			int status = 0, wpid = 0;
 			if ((wpid = waitpid(pid, &status, 0)) < 0)
 				BOOST_THROW_EXCEPTION(std::runtime_error("waitpid failed"));
@@ -63,24 +165,24 @@ namespace hide
 				_retCode = 255;
 			}
 
-			std::string out(4096, '\0');
-			fcntl(out_pipe[read_index], F_SETFL, O_NONBLOCK | O_ASYNC);
-			int ret = read(out_pipe[read_index], &out[0], out.size());
-			out.resize(ret >= 0 ? ret : 0);
+			//std::string out(4096, '\0');
+			//fcntl(out_pipe[read_index], F_SETFL, O_NONBLOCK | O_ASYNC);
+			//int ret = read(out_pipe[read_index], &out[0], out.size());
+			//out.resize(ret >= 0 ? ret : 0);
 
-			std::string err(4096, '\0');
-			fcntl(err_pipe[read_index], F_SETFL, O_NONBLOCK | O_ASYNC);
-			ret = read(err_pipe[read_index], &err[0], err.size());
-			err.resize(ret >= 0 ? ret : 0);
+			//std::string err(4096, '\0');
+			//fcntl(err_pipe[read_index], F_SETFL, O_NONBLOCK | O_ASYNC);
+			//ret = read(err_pipe[read_index], &err[0], err.size());
+			//err.resize(ret >= 0 ? ret : 0);
 
-			if (!err.empty())
-				s_logger.Debug() << "Subprocess stderr: " << err;
-			if (!out.empty())
-				s_logger.Debug() << "Subprocess stdout: " << out;
+			//if (!err.empty())
+				//s_logger.Debug() << "Subprocess stderr: " << err;
+			//if (!out.empty())
+				//s_logger.Debug() << "Subprocess stdout: " << out;
 
 			close(in_pipe[write_index]);
-			close(out_pipe[read_index]);
-			close(err_pipe[read_index]);
+			//close(out_pipe[read_index]);
+			//close(err_pipe[read_index]);
 		}
 		else
 		{
--- hide/utils/Executable.h
+++ hide/utils/Executable.h
@@ -1,9 +1,9 @@
 #ifndef HIDE_UTILS_EXECUTABLE_H
 #define HIDE_UTILS_EXECUTABLE_H
 
-
 #include <boost/optional.hpp>
 
+#include <hide/utils/IReadBuffer.h>
 #include <hide/utils/NamedLogger.h>
 #include <hide/utils/Utils.h>
 
@@ -17,9 +17,13 @@ namespace hide
 	// TODO: design an asynchronous interface
 	class Executable
 	{
+	public:
+
 	private:
 		static NamedLogger			s_logger;
 		boost::optional<int>		_retCode;
+		IReadBufferPtr				_stdout;
+		IReadBufferPtr				_stderr;
 
 	private:
 		Executable(const std::string& executable, const StringArray& parameters);
