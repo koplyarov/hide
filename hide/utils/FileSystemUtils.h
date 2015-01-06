a922d22c791663a421cb73c78d9e72e2d367fd7c
Dmitry Koplyarov <koplyarov.da@gmail.com>
2015-01-06 15:27:01 +0300

    FileSystemUtils.cpp

--- hide/utils/FileSystemUtils.h
+++ hide/utils/FileSystemUtils.h
@@ -8,46 +8,7 @@
 namespace hide
 {
 
-	boost::filesystem::path RelativePath(const boost::filesystem::path &path, const boost::filesystem::path &relative_to)
-	{
-		namespace fs = boost::filesystem;
-
-		// create absolute paths
-		fs::path p = fs::absolute(path);
-		fs::path r = fs::absolute(relative_to);
-
-		// if root paths are different, return absolute path
-		if( p.root_path() != r.root_path() )
-			return p;
-
-		// initialize relative path
-		fs::path result;
-
-		// find out where the two paths diverge
-		fs::path::const_iterator itr_path = p.begin();
-		fs::path::const_iterator itr_relative_to = r.begin();
-		while( *itr_path == *itr_relative_to && itr_path != p.end() && itr_relative_to != r.end() ) {
-			++itr_path;
-			++itr_relative_to;
-		}
-
-		// add "../" for each remaining token in relative_to
-		if( itr_relative_to != r.end() ) {
-			++itr_relative_to;
-			while( itr_relative_to != r.end() ) {
-				result /= "..";
-				++itr_relative_to;
-			}
-		}
-
-		// add remaining path
-		while( itr_path != p.end() ) {
-			result /= *itr_path;
-			++itr_path;
-		}
-
-		return result;
-	}
+	boost::filesystem::path RelativePath(const boost::filesystem::path &path, const boost::filesystem::path &relative_to);
 
 }
 
