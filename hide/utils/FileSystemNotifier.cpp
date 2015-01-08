c5c0bf01f385b52b38d04fc0c4601d26d819ee2d
Dmitry Koplyarov <koplyarov.da@gmail.com>
2015-01-09 00:50:59 +0300

    Done with FileSystemNotifier

--- hide/utils/FileSystemNotifier.cpp
+++ hide/utils/FileSystemNotifier.cpp
@@ -10,12 +10,14 @@
 #	include <sys/inotify.h>
 #endif
 
+#include <hide/utils/ListenersHolder.h>
+
 
 namespace hide
 {
 
 #ifdef HIDE_PLATFORM_POSIX
-	class FileSystemNotifier::Impl
+	class FileSystemNotifier::Impl : public ListenersHolder<IFileSysterNotifierListener>
 	{
 		HIDE_NONCOPYABLE(Impl);
 
@@ -32,19 +34,18 @@ namespace hide
 
 	public:
 		Impl()
+			: _interrupted(false)
 		{
 			_fd = inotify_init();
 			HIDE_CHECK(_fd >= 0, std::runtime_error("inotify_init failed!"));
 
 			HIDE_CHECK(pipe(_interruptPipe) == 0, std::system_error(errno, std::system_category(), "pipe failed!"));
-
-			AddPath(".");
 		}
 
 		~Impl()
 		{
 			if (close(_fd) != 0)
-				s_logger.Error() << "Could not close inotify fd!";
+				FileSystemNotifier::s_logger.Error() << "Could not close inotify fd!";
 		}
 
 		void Interrupt()
@@ -55,15 +56,14 @@ namespace hide
 			}
 			char c = 0;
 			if (write(_interruptPipe[1], &c, 1) != 1)
-				s_logger.Error() << "Could not write to _interruptPipe!";
+				FileSystemNotifier::s_logger.Error() << "Could not write to _interruptPipe!";
 		}
 
 		void AddPath(const std::string& p)
 		{
-			s_logger.Info() << "AddPath(" << p << ")";
 			HIDE_LOCK(_mutex);
 			HIDE_CHECK(_pathToWd.find(p) == _pathToWd.end(), std::runtime_error("The path '" + p + "' has already been added!"));
-			int wd = inotify_add_watch(_fd, p.c_str(), IN_ATTRIB | IN_MODIFY | IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
+			int wd = inotify_add_watch(_fd, p.c_str(), IN_MODIFY | IN_CREATE | IN_DELETE | IN_DELETE_SELF | IN_MOVED_FROM | IN_MOVED_TO);
 			HIDE_CHECK(wd >= 0, std::system_error(errno, std::system_category(), "inotify_add_watch failed!"));
 			_pathToWd.insert(std::make_pair(p, wd));
 			_wdToPath.insert(std::make_pair(wd, p));
@@ -71,7 +71,6 @@ namespace hide
 
 		void RemovePath(const std::string& p)
 		{
-			s_logger.Info() << "RemovePath(" << p << ")";
 			HIDE_LOCK(_mutex);
 			auto ptwd_it = _pathToWd.find(p);
 			HIDE_CHECK(ptwd_it != _pathToWd.end(), std::runtime_error("The path '" + p + "' has not been added!"));
@@ -85,7 +84,6 @@ namespace hide
 
 		bool ProcessEvents()
 		{
-			s_logger.Debug() << "ProcessEvents()";
 			std::array<char, sizeof(struct inotify_event) + NAME_MAX + 1>	buf;
 
 			fd_set rfds;
@@ -106,11 +104,49 @@ namespace hide
 				return true;
 			HIDE_CHECK(ret >= 0, std::system_error(errno, std::system_category(), "read failed!"));
 
-			auto event = reinterpret_cast<const struct inotify_event*>(buf.data());
-			s_logger.Warning() << "EVENT: { path: '" << event->name << "', mask: " << event->mask << " }";
+			int ofs = 0;
+			while (ofs < ret)
+			{
+				auto event = reinterpret_cast<const struct inotify_event*>(buf.data() + ofs);
+
+				auto it = _wdToPath.find(event->wd);
+
+				std::string name = std::string(event->name, event->len);
+				size_t term_null = name.find_first_of('\0');
+				name = name.substr(0, term_null);
+
+				FileSystemNotifier::s_logger.Debug() << "wd: " << it->second <<  ", name: " << name << ", mask: " << Hex(event->mask);
+
+				std::string path = name.empty() ? it->second : (it->second + "/" + name);
+
+				FileSystemNotifierEvent event_type = FileSystemNotifierEvent::Modified;
+				if ((event->mask & IN_MODIFY) != 0)
+					event_type = FileSystemNotifierEvent::Modified;
+				else if ((event->mask & IN_CREATE) != 0 || (event->mask & IN_MOVED_TO) != 0)
+					event_type = FileSystemNotifierEvent::Added;
+				else if ((event->mask & IN_DELETE) != 0 || (event->mask & IN_MOVED_FROM) != 0 || (event->mask & IN_DELETE_SELF) != 0)
+					event_type = FileSystemNotifierEvent::Removed;
+				else if ((event->mask & IN_IGNORED) != 0)
+					break;
+				else
+				{
+					FileSystemNotifier::s_logger.Warning() << "Unknown event->mask: " << Hex(event->mask);
+					break;
+				}
+
+				FileSystemNotifierTarget target = (event->mask & IN_ISDIR) != 0 ? FileSystemNotifierTarget::Directory : FileSystemNotifierTarget::File;
+
+				InvokeListeners(std::bind(&IFileSysterNotifierListener::OnEvent, std::placeholders::_1, target, event_type, path));
+
+				ofs += sizeof(struct inotify_event) + event->len;
+			}
 
 			return true;
 		}
+
+	protected:
+		void PopulateState(const IFileSysterNotifierListenerPtr& listener) const
+		{ }
 	};
 #else
 #	error No FileSystemNotifier::Impl class!
@@ -137,12 +173,26 @@ namespace hide
 	}
 
 
+	void FileSystemNotifier::AddListener(const IFileSysterNotifierListenerPtr& listener)
+	{ _impl->AddListener(listener); }
+
+
+	void FileSystemNotifier::RemoveListener(const IFileSysterNotifierListenerPtr& listener)
+	{ _impl->RemoveListener(listener); }
+
+
 	void FileSystemNotifier::AddPath(const std::string& p)
-	{ _impl->AddPath(p); }
+	{
+		s_logger.Debug() << "AddPath(" << p << ")";
+		_impl->AddPath(p);
+	}
 
 
 	void FileSystemNotifier::RemovePath(const std::string& p)
-	{ _impl->RemovePath(p); }
+	{
+		s_logger.Debug() << "RemovePath(" << p << ")";
+		_impl->RemovePath(p);
+	}
 
 
 	void FileSystemNotifier::ThreadFunc()
--- hide/utils/FileSystemNotifier.h
+++ hide/utils/FileSystemNotifier.h
@@ -11,6 +11,50 @@
 namespace hide
 {
 
+	struct FileSystemNotifierEvent
+	{
+		HIDE_ENUM_VALUES(Added, Removed, Modified);
+		HIDE_ENUM_CLASS(FileSystemNotifierEvent);
+
+		std::string ToString() const
+		{
+			switch (GetRaw())
+			{
+			case FileSystemNotifierEvent::Added:	return "Added";
+			case FileSystemNotifierEvent::Removed:	return "Removed";
+			case FileSystemNotifierEvent::Modified:	return "Modified";
+			default:								BOOST_THROW_EXCEPTION(std::runtime_error(StringBuilder() % "Unknown FileSystemNotifierEvent value: " % _val));
+			}
+		}
+	};
+
+
+	struct FileSystemNotifierTarget
+	{
+		HIDE_ENUM_VALUES(File, Directory);
+		HIDE_ENUM_CLASS(FileSystemNotifierTarget);
+
+		std::string ToString() const
+		{
+			switch (GetRaw())
+			{
+			case FileSystemNotifierTarget::File:		return "File";
+			case FileSystemNotifierTarget::Directory:	return "Directory";
+			default:									BOOST_THROW_EXCEPTION(std::runtime_error(StringBuilder() % "Unknown FileSystemNotifierTarget value: " % _val));
+			}
+		}
+	};
+
+
+	struct IFileSysterNotifierListener
+	{
+		virtual ~IFileSysterNotifierListener() { }
+
+		virtual void OnEvent(FileSystemNotifierTarget target, FileSystemNotifierEvent event, const std::string& path) { HIDE_PURE_VIRTUAL_CALL(); }
+	};
+	HIDE_DECLARE_PTR(IFileSysterNotifierListener);
+
+
 	class FileSystemNotifier
 	{
 		HIDE_NONCOPYABLE(FileSystemNotifier);
@@ -26,6 +70,9 @@ namespace hide
 	public:
 		FileSystemNotifier();
 		~FileSystemNotifier();
+
+		void AddListener(const IFileSysterNotifierListenerPtr& listener);
+		void RemoveListener(const IFileSysterNotifierListenerPtr& listener);
 
 		void AddPath(const std::string& p);
 		void RemovePath(const std::string& p);
