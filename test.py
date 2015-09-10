aadaabac8c8e1d54aea2f3bb97dd768fea1f8677
Dmitry Koplyarov <dmitry.koplyarov@iksatprof.ru>
2015-09-10 19:33:09 +0300

    Updated test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import time
+import threading
 
 sys.path.insert(0, './bin')
 import hide
@@ -9,15 +11,7 @@ class LoggerSink(hide.ILoggerSink):
     def __init__(self):
         super(LoggerSink, self).__init__()
     def PrintMessage(self, msg):
-        print msg
-
-class BuildProcessListener(hide.IBuildProcessListener):
-    def __init__(self):
-        super(BuildProcessListener, self).__init__()
-    def OnLine(self, line):
-        print "BUILD: " + line.GetText()
-    def OnFinished(self, succeeded):
-        print "BUILD " + ("SUCCEEDED" if succeeded else "FAILED")
+        print msg.ToString()
 
 hide.SetCurrentThreadName('main')
 
@@ -27,16 +21,7 @@ hide.Logger.AddSink(sink);
 hide.Logger.SetLogLevel(hide.LogLevel.Debug);
 
 p = hide.Project.CreateAuto(['.*\\bCMakeFiles\\b.*', '.*\\.git\\b.*'])
-#for f in p.GetFiles():
-        #print f.GetFilename()
 
-build_process_listener = BuildProcessListener()
-build_process_listener.__disown__()
-build_process = p.GetBuildSystem().BuildAll()
-build_process.AddListener(build_process_listener)
-del build_process
-
-#print p.GetBuildSystem().GetTargets()
-#p.GetBuildSystem().BuildFile(p.GetFileByPath('hide/lang_plugins/cpp/clang/Index.cpp'))
+time.sleep(45)
 
 hide.Logger.RemoveSink(sink);
